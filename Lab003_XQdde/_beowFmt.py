f0240f16f39b171a57e278219edf03e5e51b8f84
danielchou <daniel0422@gmail.com>
2024-10-13 22:57:33 +0800

    整理目錄

---
+++ Lab003_XQdde/_beowFmt.py
@@ -0,0 +1,261 @@
+import pandas as pd
+import math
+
+def diffRatio(r):
+    w1 , m1, q1, y1 = r["ma5"], r["ma20"], r["ma60"], r["ma240"]
+    wmqy_dscr2 = ""                              #有按照大小排列
+    dic = { "w" : w1, "m" : m1, "q" : q1, "y": y1 }  #以dict作為排列的工具
+    dic2 = { k: v for k, v in sorted( dic.items(), key=lambda item: item[1], reverse = True )}
+    wmqy_dscr2 = ",".join(dic2)
+    return wmqy_dscr2
+
+def setKbarTop(r):
+    return r["close"] if(r["kbar"] == "R") else r["open"]
+
+def setKbarBottom(r):
+    return r["close"] if(r["kbar"] == "G") else r["open"]
+
+def isCrossMA5(r):
+    top, bottom, ma, ytop = r["kbar_top"], r["kbar_bottom"], r["ma5"], r["kbar_top_y"]
+    return "C" if ( top >= ma and ma >= bottom) else "J" if(bottom >= ma and ma >= ytop) else ""
+
+def isCrossMA10(r):
+    top, bottom, ma, ytop = r["kbar_top"], r["kbar_bottom"], r["ma10"], r["kbar_top_y"]
+    return "C" if ( top >= ma and ma >= bottom) else "J" if(bottom >= ma and ma >= ytop) else ""
+
+def isCrossMA20(r):
+    top, bottom, ma, ytop = r["kbar_top"], r["kbar_bottom"], r["ma20"], r["kbar_top_y"]
+    return "C" if ( top >= ma and ma >= bottom) else "J" if(bottom >= ma and ma >= ytop) else ""
+
+def isCrossMA60(r):
+    top, bottom, ma, ytop = r["kbar_top"], r["kbar_bottom"], r["ma60"], r["kbar_top_y"]
+    return "C" if ( top >= ma and ma >= bottom) else "J" if(bottom >= ma and ma >= ytop) else ""
+
+def isCrossMA240(r):
+    top, bottom, ma, ytop = r["kbar_top"], r["kbar_bottom"], r["ma240"], r["kbar_top_y"]
+    return "C" if ( top >= ma and ma >= bottom) else "J" if(bottom >= ma and ma >= ytop) else ""
+    
+# 判斷上下彎勾 ####################################################
+def fmtGetCurvHookMa5(r):
+    rs, flagN, _n, _y = "", r["crvFlagMa5"], r["sMa5"], r["sMa5y"]
+    if (flagN == -1):
+        rs = "H" if (_n > _y and _n != _y) else "C"
+    return rs
+
+def fmtGetCurvHookMa10(r):
+    rs, flagN, _n, _y = "", r["crvFlagMa10"], r["sMa10"], r["sMa10y"]
+    if (flagN == -1):
+        rs = "H" if (_n > _y and _n != _y) else "C"
+    return rs
+
+def fmtGetCurvHookMa20(r):
+    rs, flagN, _n, _y = "", r["crvFlagMa20"], r["sMa20"], r["sMa20y"]
+    if (flagN == -1):
+        rs = "H" if (_n > _y and _n != _y) else "C"
+    return rs
+
+def fmtGetCurvHookMa60(r):
+    rs, flagN, _n, _y = "", r["crvFlagMa60"], r["sMa60"], r["sMa60y"]
+    if (flagN == -1):
+        rs = "H" if (_n > _y and _n != _y) else "C"
+    return rs
+
+def fmtGetCurvHookMa240(r):
+    rs, flagN, _n, _y = "", r["crvFlagMa240"], r["sMa240"], r["sMa240y"]
+    if (flagN == -1):
+        rs = "H" if (_n > _y and _n != _y) else "C"
+    return rs
+
+def fmtUpperShadow(r):
+    kbar_top, h = r["kbar_top"], r["high"]
+    return 0 if (kbar_top == 0) else round((h - kbar_top) / kbar_top * 100, 2)
+
+def fmtLowerShadow(r):
+    kbar_bottom, l = r["kbar_bottom"], r["low"]
+    return 0 if (l == 0) else round((kbar_bottom - l) / l * 100, 2)
+
+def fmtKbarBody(r):
+    kbar_bottom, kbar_top = r["kbar_bottom"], r["kbar_top"]
+    return 0 if (kbar_bottom ==0) else round((kbar_top - kbar_bottom) / kbar_bottom * 100, 2)
+
+def fmtAmplitude(r):
+    yc, close = r["yc"], r["close"]
+    return 0 if (yc ==0) else round((close - yc) / yc * 100, 2)
+
+def fmtCtnC(r):
+    yc, close = r["yc"], r["close"]
+    return 1 if (yc > close) else 0
+
+#描述價格往上跳空缺口
+def fmtIGap(r):
+    low, yh, h, ylow= r["low"], r["yh"], r["high"], r["yl"] 
+    gap1 = 0 if (math.isnan(yh) or math.isnan(low)) else round(low - yh, 2)
+    gap1 = gap1 if(gap1 > 0) else 0
+    
+    if (gap1 == 0):
+        gap2 = 0 if (math.isnan(ylow) or math.isnan(h)) else round(ylow - h, 2)
+        return gap2 if(gap2 > 0) else 0
+    else:
+        return gap1 
+
+def fmtSql_stockParas(r):
+    stockId, today, kbar, v, o, c, h, l, yc, iGap, amplitude, k ,d, head, body, footer = r["stockId"], r["today"], r["kbar"], r["volumn"], r["open"], r["close"], r["high"], r["low"], r["yc"], r["iGap"], r["amplitude"], r["k"], r["d"], r["upper-shadow"], r["kbar-body"], r["lower-shadow"]
+    k = 0 if (math.isnan(k)) else k
+    d = 0 if (math.isnan(d)) else d
+    # insert into dbo.StockParas (stockId, closeDate,kbar, v, o, c, h, l, yc,iGap,amplitude,k,d) values 
+    sql = f"""('{stockId}','{today}','{kbar}',{v},{o},{c},{h},{l},{yc},{head},{body},{footer},{iGap},{amplitude},{k},{d}) """
+    return sql
+
+def fmtSql_stockMA(r):
+    stockId, today, kbar, ma_dscr           = r["stockId"], r["today"], r["kbar"], r["ma_dscr"]
+    ma5, ma7, ma10, ma20, ma60, ma240       = r["ma5"], r["ma7"], r["ma10"], r["ma20"], r["ma60"], r["ma240"]
+    maF5, maF10, maF20, maF60, maF240       = r["maF5"], r["maF10"], r["maF20"], r["maF60"], r["maF240"]
+    maS5, maS10, maS20, maS60, maS240       = r["sMa5a"], r["sMa10a"], r["sMa20a"], r["sMa60a"], r["sMa240a"]   #計算斜率
+    d_ma5, d_ma10, d_ma20, d_ma60, d_ma240  = r["d_ma5"], r["d_ma10"], r["d_ma20"], r["d_ma60"], r["d_ma240"]
+    
+    # ma5 = 0 if (math.isfinite(ma5)) else ma5
+    # ma10 = 0 if (math.isfinite(ma10)) else ma10
+    # ma20 = 0 if (math.isfinite(ma20)) else ma20
+    # ma60 = 0 if (math.isfinite(ma60)) else ma60
+    # ma240 = 0 if (math.isfinite(ma240)) else ma240
+
+    # maS5 = 0 if (math.isfinite(maS5) or math.isinf(maS5)) else maS5
+    # maS10 = 0 if (math.isfinite(maS10) or math.isinf(maS10)) else maS10
+    # maS20 = 0 if (math.isfinite(maS20) or math.isinf(maS20)) else maS20
+    # maS60 = 0 if (math.isfinite(maS60) or math.isinf(maS60)) else maS60
+    # maS240 = 0 if (math.isfinite(maS240) or math.isinf(maS240)) else maS240
+
+
+    #insert into dbo.StockMA (stockId, closeDate,kbar,ma_dscr, ma5, ma7, ma10, ma20, ma60, ma240, d_ma5, d_ma10, d_ma20, d_ma60, d_ma240, maF5, maF10, maF20, maF60, maF240) values 
+    sql = f"""({stockId},'{today}','{kbar}','{ma_dscr}',{ma5},{ma7},{ma10},{ma20},{ma60},{ma240},'{d_ma5}','{d_ma10}','{d_ma20}','{d_ma60}','{d_ma240}','{maF5}','{maF10}','{maF20}','{maF60}','{maF240}',{maS5},{maS10},{maS20},{maS60},{maS240}) """
+    return sql
+
+def fmtSql_Volumn(r):
+    stockId, dt, fc, ic, dealer, yv, v, ma5, ma20, ma60 = r["stock_id"], r["dt"], r["_fc"], r["_ic"],r["_dc"], r["yv"], r["v"], r["ma5"], r["ma20"], r["ma60"]
+    #"insert into volumn(stockId,closeDate,fc,ic,dealer,accu_fc,accu_ic,k_fc,k_ic,yv,v,ma5,ma20,ma60) values 
+    sql = f"('{stockId}','{dt}',{fc},{ic},{dealer},0,0,0,0,{yv},{v},{ma5},{ma20},{ma60})"                # Python 語法解析器把 f-string
+    return sql
+
+def fmtSQL_maxMMRevenue(r):
+    stockId, nowDate, nowRvn, lastMaxDt, lastMaxV = r["stockId"], r["nowDt"], r["now"], r["max2Dt"], r["max2"]
+
+    return f"""if (select count(*) from maxMMRevenue where stockId={stockId} and nowDate='{nowDate}') = 0
+    begin insert into maxMMRevenue (stockId,nowDate,nowRvn,lastMaxDate,lastMaxRvn,createdDate) values ('{stockId}','{nowDate}',{nowRvn},'{lastMaxDt}',{lastMaxV},getdate());  end
+    """
+    
+def fmtSql_capital(r):
+    stockId, cap = r["stockId"], r["cap"]
+    return f"update stock set capital={cap},modifiedDate=getdate() where id='{stockId}'"
+
+def fmt_all_infor_stock(r):
+    id, market, open, close, estVolume, amp, jumpRate, vR, inV, vMM, vHYY = r["id"], r["market"], r["open"], r["close"], r["預估量"], r["amp"], r["jumpRate"], r["量比"], r["周轉率"], r["月量比"], r["半年量比"]
+    ss = f'"id":{id},"mk":"{market}","o":{open},"c":{close},"v":{estVolume},"amp":{amp},"jR":{jumpRate},"vR":{vR},"inV":{inV},"vMM":{vMM},"vYY":{vHYY}'
+    return ss
+
+import os
+import codecs 
+
+def write_LogFile(fileName, write_content):
+    os.makedirs(os.path.dirname(fileName), exist_ok=True)
+    f = codecs.open(fileName, mode="w", encoding="utf-8", errors="strict")
+    f.write(write_content)
+    f.close()
+
+def getLastFileDate(directory, trimStr):
+
+    # 列出目錄中的所有檔案
+    items = os.listdir(directory)
+
+    # 過濾出只有檔案的項目，並提取檔案名稱部分
+    files = [os.path.splitext(item)[0] for item in items if os.path.isfile(os.path.join(directory, item))]
+
+    # 對檔案名稱進行排序
+    sorted_files = sorted(files)
+
+    # 取得排序後的檔案名稱中的最後一個（最大的）
+    max_filename = sorted_files[-1]
+    lastFileDate = max_filename.removeprefix(trimStr) #例如：ma_20230622 只取出20230622
+    print("最新的檔案日期：", lastFileDate)
+    return lastFileDate
+
+def getLast2FileDate(directory, trimStr):
+
+    # 列出目錄中的所有檔案
+    items = os.listdir(directory)
+
+    # 過濾出只有檔案的項目，並提取檔案名稱部分
+    files = [os.path.splitext(item)[0] for item in items if os.path.isfile(os.path.join(directory, item))]
+
+    # 對檔案名稱進行排序
+    sorted_files = sorted(files)
+
+    # 取得排序後的檔案名稱中的最後一個（最大的）
+    max_filename = sorted_files[-2]
+    lastFileDate = max_filename.removeprefix(trimStr) #例如：ma_20230622 只取出20230622
+    print("次新的檔案日期：", lastFileDate)
+    return lastFileDate
+
+
+def lastFinlabStockIdList(positionData):
+    dff = positionData.iloc[-1:].transpose()
+    dff["stock"] = dff.index
+    dff.columns = ["val", "stock"]
+    df2 = dff[ (dff["val"] == True) & (dff["stock"].str.len() == 4)].dropna(axis = 0, how ='any')
+    # print(df2)
+    return df2.index.tolist()
+
+def fmtSql_stockb(r):
+    stockId, dt, v, o, c, h, l = r["id"], r["dt"], r["volume"], r["open"], r["close"], r["high"], r["low"]
+    # insert into dbo.StockParas (stockId, dt, v, o, c, h, l) values 
+    sql = f"""('{stockId}','{dt}',{v},{o},{c},{h},{l})"""
+    return sql
+
+import ftplib
+import time
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv() # 加載 .env 文件
+
+hostname = os.getenv('FTP_HOSTNAME')
+username = os.getenv('FTP_USERNAME')
+password = os.getenv('FTP_PASSWORD')
+
+def FtpFile(local_file_path, remote_file_path):
+    try:
+        # 連線到遠端FTP伺服器
+        ftp = ftplib.FTP(hostname)
+        ftp.login(username, password)
+
+        # 設定檔名的編碼格式
+        ftp.encoding = 'utf-8'
+
+        # 以二進制模式開啟本地檔案
+        with open(local_file_path, 'rb') as file:
+            # 使用FTP的STOR命令上傳檔案
+            ftp.storbinary('STOR ' + remote_file_path, file)
+
+        print(f"成功上傳檔案至 {hostname} 的 {remote_file_path}")
+    except ftplib.all_errors as e:
+        print(f"上傳失敗：{e}")
+    finally:
+        # 關閉FTP連線
+        ftp.quit()
+
+
+
+from datetime import date
+
+def trans2CSVfile(df, filePath):
+    dff = df.iloc[-1:].transpose()
+    dff["stockId"] = dff.index
+    dff.columns = ["val", "stockId"]
+    dff = dff[ (dff["stockId"].str.len() == 4) & (dff["val"] == True) ]
+
+    nowDate, ss = date.today().strftime("%Y%m%d"), ''
+    print(f"匯出共 {len(dff)} 檔股票")
+    for c in dff["stockId"].tolist():
+        ss += f"{c}.TW,"
+    write_LogFile(f"{filePath}{nowDate}.csv", ss) 
+
---
+++ Lab003_XQdde/a013_DDE.py
@@ -0,0 +1,200 @@
+import pandas as pd
+import _beowFmt as fm
+import _beowDDE as bwdde
+
+# 設置顯示選項
+# pd.set_option('display.unicode.ambiguous_as_wide', True)
+# pd.set_option('display.unicode.east_asian_width', True)
+# pd.set_option('display.width', 880)  # 設置顯示寬度
+# # pd.set_option('display.max_columns', None)  # 顯示所有列
+# pd.set_option('display.max_rows', 40)  # 顯示所有行
+# pd.set_option('display.expand_frame_repr', False)  # 不自動換行
+
+
+def process_dde_data(columns, data, sep):
+    # 打印原始數據以檢查格式
+    # print("Raw DDE data:")
+    # for item in data:
+    #     print(item) 
+
+    processed_data = []
+    for item in data:
+        # 如果數據是單個字符串，嘗試分割
+        if isinstance(item, str):
+            # 這裡使用製表符（\t）分割，您可能需要根據實際格式調整
+            processed_item = item.split(sep)
+            if len(processed_item) != len(columns):
+                print(len(processed_item),len(columns))
+                print(f"Warning: Unexpected number of fields in item: {item}")
+                continue
+        else:
+            print(f"Warning: Unexpected data type for item: {type(item)}")
+            continue
+        processed_data.append(processed_item)
+    
+    return pd.DataFrame(processed_data, columns=columns)
+
+def calcX(r):
+    volRate, turnOver, whaleSpreadRatio, yoy, mom, net = r["量比"], r["換手率%"], r["大戶差比"], r['yoy'],r['mom'],r['net']
+    volRate2, yoymom, net2 = 1, 1, 1
+
+    if (yoy > 2.1 and mom > 2.1):
+        yoymom = 2
+    if (net > 20):
+        net2 = round(net/20,1)
+    if volRate >= 1.8:
+        volRate2 = volRate
+
+    return round(volRate2 * turnOver * whaleSpreadRatio * yoymom * net2 / 100 , 1)
+
+def fmt_xq2json(r):
+    stockId, stockName,k, low, high, close, yesterdayClose, amplitude, estValue, totalValue, volRate, turnOver,\
+          roe, ioRate, market, vMa5, ln, whaleSpread, whaleSpreadRatio, \
+          cap, pe, pb, yoy, mom, net, dvd, info, shh, inv, x, mn = r["代碼"], r["商品"], r["多空"], r["最低"], r["最高"], r["成交"], r["昨收"], r["漲幅%"], r["估計量"],r["總量"], r["量比"], r["換手率%"], \
+            r["ROE%"], r["內外盤%"], r["market"], r["五日量比"], r["融資使用率%"], r["大戶差2"], r["大戶差比"], \
+            r["cap"],r["pe"],r['pb'],r['yoy'],r['mom'],r['net'],r["dvd"],r["info"],r['shh'],r['inv'],r['x'],r['mm']
+   
+    # ln = "" if (ln == '--') else ln   # Finance Used Ratio 融資使用率
+    roe = 0 if (roe == '--') else roe
+    ioRate = 0 if (ioRate == '--') else ioRate
+    estValue = 0 if (estValue == '--') else estValue
+    yoy = 0 if (yoy == '--') else yoy
+    mom = 0 if (mom == '--') else mom
+    net = 0 if (net == '--') else net
+    info = '' if (info == '-') else info
+    try:
+        if low == '0' or high == '0' or yesterdayClose == '0':
+            jmp = 0
+        else:
+            jmp = round(low - yesterdayClose,1) if amplitude > 0 else round(yesterdayClose - high,1)
+    except Exception as e:
+        print(low, high, yesterdayClose)
+        print(f"An error occurred: {e}")
+        jmp = 0
+    
+    sql = f'"id":{stockId},"n":"{stockName}","k":{k},"j":{jmp},"c":{close},"yc":{yesterdayClose},"v":{totalValue},"vE":{estValue},"amp":{amplitude},"vR":{volRate},"turOv":{turnOver},"roe":{roe},"ioR":{ioRate},"ind":"{market}","v5":{vMa5},"ln":"{ln}","wts":{whaleSpread},"wtr":{whaleSpreadRatio},"pe":{pe},"pb":{pb},"yy":{yoy},"mm":{mom},"nt":{net},"dd":"{dvd}","fo":"{info}","sh":"{shh}","iv":"{inv}","x":{x},"mn":"{mn}"'
+    return "{" + sql + "},"
+
+
+def fmt_group_json(r):
+    market, vrMean, turnOverMean, k, count, bigSum, bigCount, details = r['market'], r['平均量比'], r['換手率%'], r['多空'], r['筆數'], r['大戶買總額'],r['大戶買多的股票數量'], r['相關股票明細']
+        
+    sql = f'"market":"{market}","vmn":{vrMean},"tov":{turnOverMean},"k":{k},"cc":{count},"sum":{bigSum},"bigCC":{bigCount},"dls":"{details}"'
+    # sql = f'"market":"{market}","dls":"{details}"'
+    return "{" + sql + "},"
+
+def Main(topic, ddeDict, stock_Ids, sep):
+    service = "XQLITE"
+
+    dde_List = list(ddeDict.keys())   #將dictionary物件轉換成list欄位
+    # print(dde_List)
+    ddeItems = [f"{code}.TW-{",".join(dde_List)}" for code in stock_Ids]
+    data = bwdde.fetch_multiple_dde_data(service, topic, ddeItems)
+
+    
+    if data:
+        df = process_dde_data(dde_List, data, sep)
+        # 重命名列名
+        df = df.rename(columns=ddeDict)
+        
+
+       # 將百分比列轉換為浮點數
+        df['ID'] = df['代碼'].astype(int)
+        df['內外盤%'] = df['內外盤%'].str.replace('%', '')
+        df['漲幅%'] = df['漲幅%'].str.replace('%', '').replace('+', '')
+        df['融資使用率%'] = df['融資使用率%'].str.replace('--', '')
+
+        str2Float_columns = ['漲幅%', '成交', '昨收', '最低', '最高', '換手率%', '大單差%', '大戶差比', '量比']
+        df[str2Float_columns] = df[str2Float_columns].replace('--', '0').astype(float)        
+        df['總量'] = pd.to_numeric(df['總量'])
+        df['總量'] = df['總量'].astype(int)
+        df['多空'] = df['漲幅%'].apply(lambda x: 1 if x > 0 else 0)
+        df['大戶差2'] =df['大戶差'].apply(bwdde.to_billion)           #全部改以億為單位
+        df['五日量比'] = df.apply(lambda r: round(float(r['總量']) / float(r['五日均量']), 1) if r['估計量'] == '--' and float(r['五日均量']) != 0 else
+                           round(float(r['估計量']) / float(r['五日均量']), 1) if float(r['五日均量']) != 0 else
+                           0, axis=1)
+        
+        #--合併產業資訊--------------------------------------------------------------------------------
+        stock_group_file = r"webJson\stock_group.csv"
+        stockMarket = pd.read_csv(stock_group_file, encoding='utf-8')
+        stockMarket.columns = ["name","id","market"]
+        # print(stockMarket.head())
+        df = pd.merge(df, stockMarket, left_on="ID", right_on="id")  ## 結合股票名稱
+        # print(df)
+       
+        # df2 = df[(df['大戶差2'] > 0.7) & (df['大戶差比'] > 10)]
+        # df2 = df[(df['大戶差比'] > 15) | (df['大戶差比'] < -15)]
+        df2 = df[((df['大戶差比'] > 6) & (df['大戶差2'] >= 0.8)) | ((df['大戶差比'] < -1) & (df['大戶差2'] <= -0.6))]
+        
+        grouped = df2.groupby(['market','多空']).agg({
+            '量比': ['mean','count'],
+            '換手率%': ['mean'],
+            '大戶差2': ['sum','mean','count']
+        })
+        grouped.columns = ['平均量比', '筆數', '換手率%', '大戶買總額','大戶買平均','大戶買多的股票數量']
+        grouped = grouped.round(1)
+        
+
+        df_group = grouped[(grouped['大戶買總額'] >= 0.8) | (grouped['大戶買總額'] <= 0)].sort_values(by='大戶買平均', ascending=False)
+        # df_group = grouped[grouped['平均大戶差比'] >= 10].sort_values(by='平均量比', ascending=False)
+        # df_group = grouped[grouped['平均大戶差比'] >= 10].sort_values(by='平均量比', ascending=False)
+        # 統計題材之後，產出對應的股票名稱，以量比由大到小排列，可快速瀏覽...
+        df_group['相關股票明細'] = df2.groupby(['market','多空']).apply(lambda x: ','.join(x.sort_values(by='量比', ascending=False)['商品']))
+        df_group = df_group.reset_index() # 重置索引，使market成為一個欄位
+        df_group['json'] = df_group.apply(fmt_group_json, axis=1)
+
+        #代表性不夠的就予以排除
+        # df_filtered = df_group[~((df_group['平均量比'] <= 1.2) & (df_group['筆數'] == 1))]
+        # df_filtered = df_filtered[~((df_filtered['多空'] == 1) & (df_filtered['大戶買總額'] < 0))]
+        # print(df_group)
+
+
+        targe_file = r"webJson\currStockMarket.json"
+        ss = ''.join(df_group['json'].fillna('').astype(str))[:-1]
+        fm.write_LogFile(targe_file, f"[{ss}]")
+        fm.FtpFile(targe_file, 'static/currStockMarket.json')
+
+        #--合併財務資訊--------------------------------------------------------------------------------
+        financeData_file = r'webJson\\financeData.csv'
+        financeData = pd.read_csv(financeData_file, encoding='utf-8')
+        financeData = financeData.fillna('')
+        financeData.columns = ["id","cap","pe","pb","yoy","mom","net","dvd","info","shh","mm","inv"]
+        # print(financeData.head(60))
+        df = pd.merge(df, financeData, left_on="ID", right_on="id")  ## 結合股票名稱
+        df['x'] = df.apply(calcX, axis=1)
+        df['mm'] = df['mm'].str.replace('2024/', '') #營收月份 2024/08
+        df['json']= df.apply(fmt_xq2json, axis = 1) 
+        # print(df[df["ID"]==4768])
+
+        targe_file = r"\webJson\currentMaxValue.100.json" #產出json資料到WebJson目錄下
+        ss = ''.join(df['json'].fillna('').astype(str))[:-1]
+        fm.write_LogFile(targe_file, f"[{ss}]")     #送出到網站    
+        fm.FtpFile(targe_file, 'static/currentMaxValue.100.json')
+    else:
+        print("Failed to fetch data")
+
+
+# 追蹤股票號碼
+stock_File = r"webJson\a013_stockIds.txt"
+stock_Ids = open(stock_File, "r").read().split(",")
+dde_basic_dict={
+            'ID': '代碼', 
+            'Name': '商品',
+            'Price': '成交',
+            'TotalVolume': '總量',
+            'VolumeRatio': '量比',
+            'TurnoverRatio': '換手率%',
+            'PreClose': '昨收',
+            'PriceChangeRatio': '漲幅%',
+            'DLOrderValueDiffRatio': '大單差%',
+            'MajorOrderDif': '大戶差',
+            'MajorOrderDifRatio': '大戶差比',
+            'Low': '最低',
+            'High': '最高',
+            'ROE': 'ROE%',
+            'InOutRatio': '內外盤%',
+            'EstimatedTotalVolume': '估計量',
+            '5DayAvgVol': '五日均量',
+            'FinanceUsedRatio': '融資使用率%',
+        }
+Main("Quote", dde_basic_dict, stock_Ids, ";")
